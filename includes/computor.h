5a9f612cb1d0eeb928b112f5b75a256657010080
Emilie PHAM <epham@e3r4p18.42.fr>
2021-01-05 17:59:53 +0100

    First commit : Project architecture and Class constructor

---
+++ includes/computor.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   computor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epham <epham@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/05 16:09:20 by epham             #+#    #+#             */
+/*   Updated: 2021/01/05 16:55:18 by epham            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMPUTOR_H
+#define COMPUTOR_H
+
+#include <iostream>
+using namespace std;
+
+/*
+*** Equation format : a1 * x^0 + b1 * x^1 + c1 * x^2 = a2 * x^0 + b2 * x^1 + c2 * x^2
+*/
+
+class Computor
+{
+    private:
+        int a1;
+        int a2;
+        int b1;
+        int b2;
+        int c1;
+        int c2;
+        int degree;
+
+    public:
+        Computor ();
+
+        int parser();
+        int solver();
+        int getDegree() { return degree; }
+};
+
+#endif
---
+++ srcs/computor.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   computor.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epham <epham@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/05 16:04:15 by epham             #+#    #+#             */
+/*   Updated: 2021/01/05 16:55:06 by epham            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "computor.h"
+
+/*
+*** Class constructor
+*/
+
+Computor::Computor()
+{
+    a1 = 0;
+    a2 = 0;
+    b1 = 0;
+    b2 = 0;
+    c1 = 0;
+    c2 = 0;
+    degree = 0;
+}
+
+/*
+*** Class parser
+*/
+
+int Computor::parser()
+{
+    return 0;
+}
+
+/*
+*** Class solver
+*/
+
+int Computor::solver()
+{
+    return 0;
+}
